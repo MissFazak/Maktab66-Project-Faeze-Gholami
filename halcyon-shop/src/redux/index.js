fbd567502997eb24408211ac034188ad8bd0a6b0
missfazak <faezegholami97@gmail.com>
2022-05-16 15:59:21 +0430

    feat: order page add redux

--- halcyon-shop/src/redux/index.js
+++ halcyon-shop/src/redux/index.js
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import itemsReducer from './productSlice'
+import orderReducer from './orderSlice'
 
 export const store = configureStore({
   devTools:true,
   reducer:{
       user:userReducer,
-      items:itemsReducer
+      items:itemsReducer,
+      orders:orderReducer
       // product:productReducer
   }
 })

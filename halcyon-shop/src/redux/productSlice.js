63df9daff2fd095f1f84d5b6bed8e7b5eb071429
missfazak <faezegholami97@gmail.com>
2022-05-16 11:12:29 +0430

    feat: make product slice

--- halcyon-shop/src/redux/productSlice.js
+++ halcyon-shop/src/redux/productSlice.js
@@ -1,14 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from 'axios'
 
 export const productSlice = createSlice({
     name:'product',
-    initialState:{},
+    initialState:{
+        items:[]
+    },
     reducers:{
-        setUser:(state,action)=>{
+        setItem:(state,action)=>{
             return action.payload
         },
         logout:state=>{
             return{}
         }
     }
+})
+
+export const {setItem} = productSlice.actions
+export default productSlice.reducer
+
+const api = axios.create({
+    baseURL:"http://localhost:3002/"
 })

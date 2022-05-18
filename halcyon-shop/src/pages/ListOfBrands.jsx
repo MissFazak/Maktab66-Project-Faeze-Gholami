934983b7c9d7dad02c451e6940f91122d77cd300
missfazak <faezegholami97@gmail.com>
2022-05-18 20:21:45 +0430

    feat: add sideBar first edit

--- halcyon-shop/src/pages/ListOfBrands.jsx
+++ halcyon-shop/src/pages/ListOfBrands.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchItems, itemsSelector } from "../redux/productSlice";
+import { fetchCategory, categorySelector } from "../redux/categorySlice";
+import { Link } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Box } from "@mui/material";
 
 export default function ListOfBrands() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  const dispatch = useDispatch();
+  const { items } = useSelector(itemsSelector);
+  const { category } = useSelector(categorySelector);
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchCategory());
+  }, [dispatch]);
   return (
-    <div>ListOfBrands</div>
-  )
+    <div>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          position: "relative",
+          overflow: "auto",
+          maxHeight: 300,
+          "& ul": { padding: 0 },
+        }}
+        className="sidebar"
+      >
+        {Object.values(category).map((item) => (
+          <List
+            sx={{
+              direction: "ltr",
+
+              width: "100%",
+              maxWidth: 360,
+              bgcolor: "background.paper",
+            }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton onClick={handleClick}>
+              <ListItemText primary={item.name} />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              {Object.values(items).map((el) => {
+                if (item.id == el.category) {
+                  return (
+                    <List component="div" disablePadding>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary={el.name} />
+                      </ListItemButton>
+                    </List>
+                  );
+                }
+              })}
+            </Collapse>
+          </List>
+        ))}
+      </Box>
+    </div>
+  );
 }

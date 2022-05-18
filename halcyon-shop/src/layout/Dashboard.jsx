1cf45e961e9d57e2d9fcaa1af3a2c6999518bfc1
missfazak <faezegholami97@gmail.com>
2022-05-18 16:31:23 +0430

    fix:handle outlet of managemaent

--- halcyon-shop/src/layout/Dashboard.jsx
+++ halcyon-shop/src/layout/Dashboard.jsx
@@ -4,17 +4,13 @@ import ManagePage from '../pages/ManagePage'
 import ManageOfSP from '../pages/ManageOfSP'
 import OrderPage from '../pages/OrderPage'
 
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route, Outlet } from "react-router-dom";
 
 export default function Dashboard() {
   return (
     <div>
       <DashboardHead />
-      <Routes>
-        <Route path="manage-page" element={<ManagePage/>} />
-        <Route path="manage-of-sp" element={<ManageOfSP/>}/>
-          <Route path="order-page" element={<OrderPage/>}/>
-      </Routes>
+      <Outlet/>
     </div>
   );
 }

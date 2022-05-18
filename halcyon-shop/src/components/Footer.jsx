be327c46abe15a0c6a431038e98746887e6b597c
missfazak <faezegholami97@gmail.com>
2022-05-18 17:46:15 +0430

    feat and style: making footer

--- halcyon-shop/src/components/Footer.jsx
+++ halcyon-shop/src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <h1>قدرت گرفته توسط مکتب شریف</h1>
+      <p>طراحی این سایت انحصارا توسط مهندس فائزه غلامی می باشد</p>
+    </div>
   )
 }

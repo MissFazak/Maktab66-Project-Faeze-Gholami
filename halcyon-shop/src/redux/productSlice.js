e3cf875e4e734aa9260d9ebc92cd3df9a7096d99
missfazak <faezegholami97@gmail.com>
2022-05-20 15:47:39 +0430

    fix: add axios to api

--- halcyon-shop/src/redux/productSlice.js
+++ halcyon-shop/src/redux/productSlice.js
@@ -1,33 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from 'axios'
-import { useMemo } from "react";
+import { api } from "./api";
 
 const productSlice = createSlice({
-    name:"items",
-    initialState:{
-        items:[]
+  name: "items",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    setItems: (state, { payload }) => {
+      return { items: [...payload] };
     },
-    reducers:{
-        setItems:(state,{payload})=>{
-            return {items:[...payload]}
-        }
-        
-    }
+  },
 });
 
-export const {setItems} = productSlice.actions
-export const itemsSelector =(state) =>state.items
-export default productSlice.reducer
-// const limit = useMemo(() => 3, []);
-// const [activePage, setActivePage] = useState(1);
-const api = axios.create({
-    baseURL:"http://localhost:3002/",
-    withCredentials:false,
-})
+export const { setItems } = productSlice.actions;
+export const itemsSelector = (state) => state.items;
+export default productSlice.reducer;
 
-export function fetchItems(){
-    return async (disptch)=>{
-        api.get(`/products`).then((res)=>disptch(setItems(res.data)))
-        
-    }
+export function fetchItems() {
+  return async (disptch) => {
+    api.get(`/products`).then((res) => disptch(setItems(res.data)));
+  };
 }

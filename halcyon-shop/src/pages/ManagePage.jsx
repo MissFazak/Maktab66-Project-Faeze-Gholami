19e675d56eb9be150b51d1be735ed266e93f022e
missfazak <faezegholami97@gmail.com>
2022-05-21 16:44:32 +0430

    fix: add category

--- halcyon-shop/src/pages/ManagePage.jsx
+++ halcyon-shop/src/pages/ManagePage.jsx
@@ -2,22 +2,38 @@ import * as React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchItems, itemsSelector } from "../redux/productSlice";
+import { fetchCategory, categorySelector } from "../redux/categorySlice";
 import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 
 export default function CustomizedTables() {
   const dispatch = useDispatch();
   const { items } = useSelector(itemsSelector);
-  
+  const { category } = useSelector(categorySelector);
+
+
   useEffect(() => {
     dispatch(fetchItems());
   }, []);
+  useEffect(() => {
+    dispatch(fetchCategory());
+  }, []);
 
   const columns = [
-    { field: "id", headerName: "ID", width: 200 },
+    { field: "id", headerName: "ID", width: 10 
+  },
     {
       field: "thumbnail",
       headerName: "تصویر",
-      width: 150,
+      width: 50,
+     renderCell: (params)=>{
+       console.log(params.row.thumbnail);
+        return (
+          <div>
+            <img style={{width:'30px'}} src={params.row.thumbnail} alt='' />
+          </div>
+        )
+      }
     },
     {
       field: "name",
@@ -30,19 +46,38 @@ export default function CustomizedTables() {
       width: 110,
     },
     {
-      field: "but",
+      field: "action",
       headerName: "",
-      width: 160,
+      width: 200,
+      sortable: false,
+      renderCell: () => {
+        const onClick = (e) => {
+          e.stopPropagation();
+          return alert('hi');
+        };
+        return (
+          <>
+            <Button onClick={onClick}>ویرایش</Button>
+            <Button onClick={onClick}>حذف</Button>
+          </>
+        );
+      },
     },
   ];
   const rows = items.map((item) => {
-    return { id: item.id,thumbnail:item.thumbnail ,name: item.name, category: item.category ,but:<h1>hi</h1>};
+    return {
+      id: item.id,
+      thumbnail: `http://localhost:3002/files/${item.thumbnail}`,
+      name: item.name,
+      category: category.map(el=>{if(el.id==item.category){return el.name}}),
+    };
   });
-  
-  console.log(rows);
-
   return (
-    <div style={{ width: "100%" }}>
+    <div className="managePage">
+      <div className="topTable">
+        <h3>مدیریت موجودی و قیمت‌ها</h3>
+        <Button variant="contained" color="primary">ذخیره</Button>
+      </div>
       <DataGrid rows={rows} columns={columns} autoHeight pageSize={5}/>
     </div>
   );

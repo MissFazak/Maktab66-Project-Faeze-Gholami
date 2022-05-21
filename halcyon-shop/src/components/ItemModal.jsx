e1aa097cba718b37a025f866e1d0f624f4599f0c
missfazak <faezegholami97@gmail.com>
2022-05-21 19:17:26 +0430

    feat: add text editor

--- halcyon-shop/src/components/ItemModal.jsx
+++ halcyon-shop/src/components/ItemModal.jsx
@@ -1,21 +1,24 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { FormControl, InputLabel, Select, TextField } from '@mui/material';
-import SelectInput from '@mui/material/Select/SelectInput';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { FormControl, InputLabel, Select, TextField } from "@mui/material";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  height:'300px',
+  overflow:'auto'
 };
 
 export default function BasicModal() {
@@ -25,7 +28,9 @@ export default function BasicModal() {
 
   return (
     <div>
-      <Button onClick={handleOpen} variant="contained" color="primary">افزودن</Button>
+      <Button onClick={handleOpen} variant="contained" color="primary">
+        افزودن
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -33,23 +38,26 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-            <Typography>تصویر کالا</Typography>
-            <input type='file'/>
-          <Typography id="modal-modal-title" variant="h6" component="h6">
+          <Typography>تصویر کالا</Typography>
+          <input type="file" style={{width:'100%'}}/>
+          <Typography id="modal-modal-title">
             نام کالا:
           </Typography>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <FormControl>
-                <Typography>دسته بندی</Typography>
-                <InputLabel>انتخاب کنید</InputLabel>
-                <Select>
-                    
-                </Select>
-            </FormControl>
+          <TextField id="outlined-basic" label="نام کالا" variant="outlined" sx={{width:'100%'}} />
+          <FormControl sx={{width:'100%'}}>
+            <Typography>دسته بندی</Typography>
+            <Select></Select>
+          </FormControl>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            توضیحات: 
+            توضیحات:
           </Typography>
-          
+          <Editor
+            // editorState={editorState}
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            // onEditorStateChange={this.onEditorStateChange}
+          />
         </Box>
       </Modal>
     </div>

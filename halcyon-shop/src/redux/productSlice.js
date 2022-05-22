60dbe8342e1a3222c34790ee976d36f126143480
missfazak <faezegholami97@gmail.com>
2022-05-22 11:26:40 +0430

    fix: add function for edit and delete

--- halcyon-shop/src/redux/productSlice.js
+++ halcyon-shop/src/redux/productSlice.js
@@ -7,23 +7,46 @@ const productSlice = createSlice({
     items: [],
   },
   reducers: {
-    setItems: (state, { payload }) => {
-      return { items: [...payload] };
+    setItems: (state, action) => {
+      return { items: [...action.payload] };
     },
     addItems: (state, action) => {
-      state.value.push(action.payload);
+      state.push(action.payload);
     },
-    removeItem: () => {},
-    editItem: () => {},
+    removeItem: (state,action) => {
+      let index = state.findIndex(({id})=>id===action.payload.id)
+      state.splice(index,1)
+    },
+    editItem: (state, action) => {
+      const index  = state.findIndex(tutorial =>tutorial.id ===action.payload.id)
+      state[index] = {
+        ...state[index],
+        ...action.payload
+      }
+    },
   },
 });
 
-export const { setItems,addItems } = productSlice.actions;
+export const { setItems, addItems,removeItem,editItem } = productSlice.actions;
 export const itemsSelector = (state) => state.items;
 export default productSlice.reducer;
 
 export function fetchItems() {
   return async (disptch) => {
     tutorialService.getProducts().then((res) => disptch(setItems(res.data)));
+  };
+}
+
+export function updateItems() {
+  return async ({ id, data }) => {
+    const res = await tutorialService.updateProduct(id, data);
+    return res.data;
+  };
+}
+
+export function deleteItem() {
+  return async ({ id }) => {
+    await tutorialService.removeProduct(id);
+    return { id };
   };
 }

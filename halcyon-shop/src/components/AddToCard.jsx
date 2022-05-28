f8b236a8b50692ef4903fb5774b9273537d3d17e
missfazak <faezegholami97@gmail.com>
2022-05-28 20:33:23 +0430

    feat:add function

---
+++ halcyon-shop/src/components/AddToCard.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function AddToCard() {
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{ padding: "5px", margin: "10px" }}
+    >
+      افزودن به سبد خرید
+    </Button>
+  );
+}

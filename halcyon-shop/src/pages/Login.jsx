ed14d82d57b502152d84dc5da8d6be4f8b2140d3
missfazak <faezegholami97@gmail.com>
2022-05-28 15:37:58 +0430

    feat: add alert for empty input

--- halcyon-shop/src/pages/Login.jsx
+++ halcyon-shop/src/pages/Login.jsx
@@ -7,31 +7,41 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import * as Yup from "yup";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [icon, setIcon] = useState(false);
-  
-  
+
   const formik = useFormik({
     initialValues: {
       username: "",
       password: "",
     },
+
+    validationSchema: Yup.object({
+      username: Yup.string().required("وارد کردن نام کاربری الزامی است!"),
+      password: Yup.string().required("وارد کردن کلمه عبور الزامی است!"),
+    }),
     onSubmit: (values) => {
       dispatch(setUser(values));
-      axios.get("http://localhost:3002/whoami").then((res) => setUser(res.data));
-      axios.post("http://localhost:3002/auth/login", formik.values).then((res) => {
-            console.log(res.status);
-            if (res.status === 200) {
-              localStorage.setItem("token", res.data.token);
-              navigate("..//dashboard/manage-page");
-            }
-          });
+      axios
+        .get("http://localhost:3002/whoami")
+        .then((res) => setUser(res.data));
+      axios
+        .post("http://localhost:3002/auth/login", formik.values)
+        .then((res) => {
+          if (res.status === 200) {
+            localStorage.setItem("token", res.data.token);
+            navigate("..//dashboard/manage-page");
+          } else {
+            alert("hi");
+          }
+        });
     },
   });
- 
+
   return (
     <div className="loginBody">
       <div className="loginForm">
@@ -41,18 +51,25 @@ const Login = () => {
             name="username"
             type="text"
             placeholder="Username"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.username}
           />
-      
+          {formik.touched.username && formik.errors.username ? (
+            <div>{formik.errors.username}</div>
+          ) : null}
           <input
             id="password"
             name="password"
             type={icon ? "text" : "password"}
             placeholder="Password"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.password}
-          />
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div>{formik.errors.password}</div>
+            ) : null}
           <div className="iconHolder">
             {icon ? (
               <VisibilityOffIcon
@@ -63,10 +80,7 @@ const Login = () => {
               <VisibilityIcon className="icons" onClick={() => setIcon(true)} />
             )}
           </div>
-      
-          <button type="submit">
-            ورود
-          </button>
+          <button type="submit">ورود</button>
         </form>
       </div>
     </div>

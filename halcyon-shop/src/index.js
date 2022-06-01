eacd13affea3e784d47d628dbaf02862b68a40b8
missfazak <faezegholami97@gmail.com>
2022-06-01 17:04:15 +0430

    feat: install toasty

--- halcyon-shop/src/index.js
+++ halcyon-shop/src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./components/theme";
 import { Provider } from "react-redux";
 import { store } from "./redux";
+import {ToastContainer} from 'react-toastify';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+      <ToastContainer/>
         <App />
       </BrowserRouter>
     </ThemeProvider>

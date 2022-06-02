e918b7daeac1e3e6db0693965d5a87253972b358
missfazak <faezegholami97@gmail.com>
2022-06-03 00:17:09 +0430

    feat: add data picker

--- halcyon-shop/src/pages/OrderForm.jsx
+++ halcyon-shop/src/pages/OrderForm.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { useFormik } from "formik";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import AdapterJalali from "@date-io/date-fns-jalali";
 
 const OrderForm = () => {
+  const [value, setValue] = React.useState(null);
+  const timeStamp = Date.parse(value) / 1000;
+  console.log(timeStamp);
+
+  function disablePrevDates(startDate) {
+    const startSeconds = Date.parse(startDate);
+    return (date) => {
+      return Date.parse(date) < startSeconds;
+    };
+  }
+  const startDate = new Date()
+  startDate.setDate(startDate.getDate() + 3);
+
+  const data = {
+    coust
+  }
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -16,45 +36,60 @@ const OrderForm = () => {
   });
   return (
     <div className="orderBody">
-        <div className="orderForm">
-          <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="firstName">نام</label>
-            <input
-              id="firstName"
-              name="firstName"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.firstName}
+      <div className="orderForm">
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">نام</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+          <label htmlFor="lastName">نام خانوادگی</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+          <label htmlFor="email">آدرس</label>
+          <input
+            id="address"
+            name="address"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <label htmlFor="phoneNumber">تلفن همراه</label>
+          <input
+            id="phoneNumber"
+            name="phoneNumber"
+            type="number"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <label htmlFor="date">تاریخ تحویل</label>
+          <LocalizationProvider dateAdapter={AdapterJalali}>
+            <DatePicker
+              id="date"
+              name="date"
+              value={value}
+              shouldDisableDate={disablePrevDates(startDate)}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+              renderInput={(params) => (
+                <TextField sx={{ borderBottom: "none" }} {...params} />
+              )}
             />
-            <label htmlFor="lastName">نام خانوادگی</label>
-            <input
-              id="lastName"
-              name="lastName"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.lastName}
-            />
-            <label htmlFor="email">آدرس</label>
-            <input
-              id="address"
-              name="address"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-            <label htmlFor="phoneNumber">تلفن همراه</label>
-            <input
-              id="phoneNumber"
-              name="phoneNumber"
-              type="number"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-          </form>
-            <Button variant="contained" color="success" type="submit">
-              پرداخت
-            </Button>
-        </div>
+          </LocalizationProvider>
+        </form>
+        <Button variant="contained" color="success" type="submit">
+          پرداخت
+        </Button>
+      </div>
     </div>
   );
 };

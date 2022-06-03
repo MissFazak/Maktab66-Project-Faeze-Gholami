036e8c6f7b5ad3655e41a7928bebe4eb271eed7d
missfazak <faezegholami97@gmail.com>
2022-06-03 04:30:45 +0430

    feat: add modal and add to deliverd

---
+++ halcyon-shop/src/components/StatusModal.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { DataGrid } from '@mui/x-data-grid';
+import { v4 as uuidv4 } from 'uuid';
+import service from '../redux/http';
+
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function StatusModal({props}) {
+    let moment = require("moment-jalaali");
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  console.log(props);
+  const columns = [
+    { field: "id", headerName: "ID", width: 10 },
+    { field: "name", headerName: "نام کالا", width: 150 },
+    { field: "price", headerName: "قیمت", width: 110 },
+    { field: "count", headerName: "تعداد", width: 200 },
+  ]
+
+  const rows = props?.orderItems.map(item=>{
+      return { id: uuidv4(), name: item.name, price: item.price, count: item.quantity }
+  })
+
+  const handleStatus = () => {
+    service.updateOrder(props.id, { orderStatus: "6" });
+  }
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>وضعیت سفارش</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography>نام مشتری : {props?.customerDetail?.firstName+' '+props?.customerDetail?.lastName}</Typography>
+          <Typography>آدرس: {props?.customerDetail?.address}</Typography>
+          <Typography>شماره تماس : {props?.customerDetail?.phoneNumber}</Typography>
+          <Typography>زمان تحویل : {moment(props?.delivery).format("jYYYY/jM/jD")}</Typography>
+          <Typography>زمان سفارش : {moment(props?.orderDate).format("jYYYY/jM/jD")}</Typography>
+          <DataGrid autoHeight rows={rows} columns={columns}/>
+          <Button variant="contained"
+            color="success" onClick={handleStatus}>تحویل سفارش</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
